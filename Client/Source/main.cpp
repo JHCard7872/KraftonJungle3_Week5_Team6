0d94a7b9f81d2494d6ceb42a197c0c47beebea8c
오준혁 <74858961+JHCard7872@users.noreply.github.com>
2026-04-07 17:10:24 +0900

    fix: 해상도 오류 수정

--- Client/Source/main.cpp
+++ Client/Source/main.cpp
@@ -3,6 +3,7 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 {
+	SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 	// Windows 런처에 넘길 클라이언트 실행 설정을 구성한다.
 	FEngineLaunchConfig Config;
 	Config.Title = L"Jungle Client";
